bbaf227d25767d809dd58fcdb39cd531ae916fcb
Jayden Thrasher <JaydenThrasher@gmail.com>
2019-10-10 18:08:01 -0400

    Add date module

--- helloWorld.js
+++ helloWorld.js
@@ -1,6 +1,8 @@
 let http = require('http');
+let dt = require('./myFirstModule')
 
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('Hello World!');
+  res.write("The date and time are currently: " + dt.myDateTime());
+  res.end();
 }).listen(8080);
